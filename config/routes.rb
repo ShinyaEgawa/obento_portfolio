5e3e5c614aa582f4390b20a6a99357ead5dfa339
Shinya Egawa <shin_mc5to13jp_craft@icloud.com>
2019-11-01 00:27:25 +0900

    Login function and page appearance correction

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'top_pages#home'
+  get 'sessions/new', to: 'sessions#new'
   get '/about', to: 'top_pages#about'
   get '/service', to: 'top_pages#service'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
