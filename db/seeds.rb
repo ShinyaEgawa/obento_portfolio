4ae2ff89ca852980c4ac77493158f57834bbf79b
Shinya Egawa <shin_mc5to13jp_craft@icloud.com>
2019-11-30 23:47:33 +0900

    Correction of Follow/Unfollow

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ end
 
 # Posts
 users = User.order(:created_at).take(6)
-50.times do
+3.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
